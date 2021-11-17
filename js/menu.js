f61c43d2294eff70bd26f074ab0a5bcce609bcfc
Mithlesh Kumar <mithlesh.9m@gmail.com>
2021-11-18 00:03:32 +0530

    fixed linter error

--- js/menu.js
+++ js/menu.js
@@ -1,15 +1,14 @@
-const closeBtn =  document.querySelector('.close');
+const closeBtn = document.querySelector('.close');
 const modal = document.querySelector('.modal');
 const loginBtn = document.querySelector('.hamburger');
-const logo = document.querySelector('.logo-main')
 
 closeBtn.addEventListener('click', () => {
-    modal.style.display = 'none';
-    loginBtn.style.display = 'block';
-  });
+  modal.style.display = 'none';
+  loginBtn.style.display = 'block';
+});
 
-  loginBtn.addEventListener('click', () => {
-    loginBtn.style.display = 'none';
-    modal.style.display = 'block';
-    document.querySelector('body').style.overflow = 'none'
-  });
+loginBtn.addEventListener('click', () => {
+  loginBtn.style.display = 'none';
+  modal.style.display = 'block';
+  document.querySelector('body').style.overflow = 'none';
+});
